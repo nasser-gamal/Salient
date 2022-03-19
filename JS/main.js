1c9876b8027cc564cebb432aea9872f64996be43
nasser-gamal <98104328+nasser-gamal@users.noreply.github.com>
2022-03-19 23:01:38 +0200

    Upload The Design

--- JS/main.js
+++ JS/main.js
@@ -40,5 +40,27 @@ window.addEventListener("scroll", () => {
       prog.style.width = prog.dataset.width;
     });
   };
-})
+});
 
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  spaceBetween: 10,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 4,
+      spaceBetween: 40,
+    },
+    1024: {
+      slidesPerView: 5,
+      spaceBetween: 10,
+    },
+  },
+});
