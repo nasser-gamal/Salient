7a353869a43027384b0c692e71b7c1c545472750
nasser-gamal <98104328+nasser-gamal@users.noreply.github.com>
2022-03-20 13:15:40 +0200

    Upload The Deisgn

--- JS/main.js
+++ JS/main.js
@@ -42,21 +42,25 @@ window.addEventListener("scroll", () => {
   };
 });
 
-var swiper = new Swiper(".mySwiper", {
+var swiper = new Swiper(".swip", {
   slidesPerView: 1,
   spaceBetween: 10,
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
   },
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
   breakpoints: {
     640: {
-      slidesPerView: 2,
+      slidesPerView: 1,
       spaceBetween: 20,
     },
     768: {
       slidesPerView: 4,
-      spaceBetween: 40,
+      spaceBetween: 20,
     },
     1024: {
       slidesPerView: 5,
